56c0bfccbe8f961b2b429bb96a4a351ca9ef5230
Kevin <kevin.ortiz.software@gmail.com>
2025-10-16 15:52:54 -0400

    updated push with dark mode active

--- content.js
+++ content.js
@@ -1,44 +1,13 @@
-console.log("hello world");
-console.log("hello world2");
-console.log("Hello world service worker");
-console.log("testing something");
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "dark_mode_on") {
+    console.log("hello");
 
-
-// the darkmodeButton when clicks will turn the body darkmode
-const darkButton = document.getElementById("dark_mode_btn");
-darkButton.addEventListener("click", darkModeClick);
-
-function darkModeClick(){
-    console.log("hello world")
-const body = document.querySelector("body");
-// console.log(`body`, body);
-// console.log("body[0]", body[0]);
-// console.log("body[0] base uri", body[0].baseURI);
-body.style.backgroundColor = "rgb(32,32,32)";
-}
-
-
-const pTags = document.querySelectorAll("p");
-// console.log('pTag', pTags);
-// console.log(`pTag[0]`, pTags[0]);
-pTags.style.color = "white";
-// console.log(`ptags spread`, ...pTags)
-// ...pTags.style.color = "white";
-// pTags.forEach(p => {
-//     p.style.color = 'white';
-// });
-// pTags.forEach((element) =>  {
-//     element.style.color = "white";
-// });
-
-// ...array
-
-changePTags = pTags.forEach((element) =>  {
+    document.body.style.backgroundColor = "black";
+    Array.from(pTags).forEach((element) => {
     element.style.color = "white";
+    });
+
+
+    
+  }
 });
-
-
-
-// ...array
-
-
---
+++ popup.js
@@ -0,0 +1,57 @@
+// console.log("hello world");
+// console.log("hello world2");
+// console.log("Hello world service worker");
+// console.log("testing something");
+
+// // the darkmodeButton when clicks will turn the body darkmode
+// const darkButton = document.getElementById("dark_mode_btn");
+// darkButton.addEventListener("click", darkModeClick);
+
+// function darkModeClick(){
+//     console.log("hello world")
+// const body = document.querySelector("body");
+// // console.log(`body`, body);
+// // console.log("body[0]", body[0]);
+// // console.log("body[0] base uri", body[0].baseURI);
+// body.style.backgroundColor = "rgb(32,32,32)";
+// }
+
+// // console.log('pTag', pTags);
+// // console.log(`pTag[0]`, pTags[0]);
+// pTags.style.color = "white";
+// // console.log(`ptags spread`, ...pTags)
+// // ...pTags.style.color = "white";
+// // pTags.forEach(p => {
+// //     p.style.color = 'white';
+// // });
+// // pTags.forEach((element) =>  {
+// //     element.style.color = "white";
+// // });
+
+// // ...array
+
+// changePTags = pTags.forEach((element) =>  {
+//     element.style.color = "white";
+// });
+
+// Array.from(pTags).forEach((element) => {
+//   element.style.color = "white";
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("dark_mode_btn");
+  
+const pTags = document.querySelectorAll("p");
+  
+  button.addEventListener("click", function () {
+    // Get the active tab and send message to content script
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs[0]) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "dark_mode_on" });
+      }
+    });
+  });
+
+
+
+});
