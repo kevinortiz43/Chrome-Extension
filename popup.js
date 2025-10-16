939a538e7f472ac01b1c65e3bbfefe3b4f78bc87
Kevin <kevin.ortiz.software@gmail.com>
2025-10-16 19:25:12 -0400

    Update

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const fontButton = document.getElementById("lexend_font_btn");
+  const darkButton = document.getElementById("dark_mode_btn");
+  const focusButton = document.getElementById("focus_mode_btn");
 
   fontButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -9,17 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  const button = document.getElementById("dark_mode_btn");
-
-  button.addEventListener("click", function () {
+  darkButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       if (tabs[0]) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "dark_mode_on" });
       }
     });
   });
-
-  const focusButton = document.getElementById("focus_mode");
 
   focusButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
