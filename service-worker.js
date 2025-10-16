baa1f34e3122ed7f9886f18e92cd91ba5a1c50d0
Kevin <kevin.ortiz.software@gmail.com>
2025-10-16 12:29:32 -0400

    We got body background turning into psuedo darkmode (black) working.
    Working on making ptags white for psuedo darkmode
    
    Co-authored-by: katygus <katygus@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,27 @@
-console.log('hello world');
-console.log('hello world2');
-console.log("Hello world service worker")
-console.log("testing something")
-//   "background": {
-//     "service_worker": "service-worker.js"
-//   }
+console.log("hello world");
+console.log("hello world2");
+console.log("Hello world service worker");
+console.log("testing something");
+
+const body = document.getElementsByTagName("body");
+// console.log(`body`, body);
+// console.log("body[0]", body[0]);
+// console.log("body[0] base uri", body[0].baseURI);
+body[0].style.backgroundColor = "black";
+
+const pTags = document.getElementsByTagName("p"); 
+// console.log('pTag', pTags);
+// console.log(`pTag[0]`, pTags[0]);
+// pTags[0].style.color = "white";
+// console.log(`ptags spread`, ...pTags)
+// ...pTags.style.color = "white";
+
+pTags.forEach((element) =>  {
+    element.style.color = "white";
+});
+
+
+
+// ...array
+
+
