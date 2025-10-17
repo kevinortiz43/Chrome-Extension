f344b4d96440a851c8c5fe0b1fb97d3aae024b6a
Kevin <kevin.ortiz.software@gmail.com>
2025-10-17 11:51:34 -0400

    Changing title to readable

--- content.js
+++ content.js
@@ -1,35 +1,25 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  const newBlack = 'rgb(32,32,32)';
-  const linkColor = 'rgb(135, 206, 250)';
-  const textColor = "white"
+  const newBlack = "rgb(32,32,32)";
 
-  if (request.action === 'dark_mode_on') {
-    const pTags = document.querySelectorAll('p');
-
-    const aTags = document.querySelectorAll('a');
-
-    const divTags = document.querySelectorAll('div');
-
-    const spanTags = document.querySelectorAll('span');
-
-    const h1Tags = document.querySelectorAll('h1');
-
-    const h2Tags = document.querySelectorAll('h2');
-
-    const h3Tags = document.querySelectorAll('h3');
-
-    const h4Tags = document.querySelectorAll('h4');
-
-    const h5Tags = document.querySelectorAll('h5');
-
+  if (request.action === "dark_mode_on") {
+    console.log("hello");
+    const pTags = document.querySelectorAll("p");
     document.body.style.backgroundColor = newBlack;
     Array.from(pTags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
 
+    const h1Tags = document.querySelectorAll("h1");
+
     Array.from(h1Tags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
+
+    const aTags = document.querySelectorAll("a");
+
+    const divTags = document.querySelectorAll("div");
+
+    console.log("div", divTags);
 
     Array.from(divTags).forEach((element) => {
       element.style.backgroundColor = newBlack;
@@ -37,114 +27,100 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
     Array.from(aTags).forEach((element) => {
       element.style.backgroundColor = newBlack;
-      element.style.color = linkColor;
+      element.style.color = 'rgb(135, 206, 250)';
     });
 
+    const h2Tags = document.querySelectorAll("h2");
     Array.from(h2Tags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
 
+    const h3Tags = document.querySelectorAll("h3");
     Array.from(h3Tags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
 
+    const spanTags = document.querySelectorAll("span");
     Array.from(spanTags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
 
+    const h4Tags = document.querySelectorAll("h4");
     Array.from(h4Tags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
 
+    const h5Tags = document.querySelectorAll("h5");
     Array.from(h5Tags).forEach((element) => {
-      element.style.color = textColor
+      element.style.color = "white";
     });
   }
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'lexend_on') {
-    console.log('hello');
+  if (request.action === "lexend_on") {
+    console.log("hello");
 
-    const pTags = document.querySelectorAll('p');
+    const pTags = document.querySelectorAll("p");
     Array.from(pTags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const h1Tags = document.querySelectorAll('h1');
+    const h1Tags = document.querySelectorAll("h1");
     Array.from(h1Tags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const aTags = document.querySelectorAll('a');
+    const aTags = document.querySelectorAll("a");
     Array.from(aTags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const h2Tags = document.querySelectorAll('h2');
+    const h2Tags = document.querySelectorAll("h2");
     Array.from(h2Tags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const h3Tags = document.querySelectorAll('h3');
+    const h3Tags = document.querySelectorAll("h3");
     Array.from(h3Tags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const spanTags = document.querySelectorAll('span');
+    const spanTags = document.querySelectorAll("span");
     Array.from(spanTags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const h4Tags = document.querySelectorAll('h4');
+    const h4Tags = document.querySelectorAll("h4");
     Array.from(h4Tags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
 
-    const h5Tags = document.querySelectorAll('h5');
+    const h5Tags = document.querySelectorAll("h5");
     Array.from(h5Tags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
-    });
-
-    const h6Tags = document.querySelectorAll('h6');
-    Array.from(h6Tags).forEach((element) => {
-      element.style.fontFamily = 'Lexend';
-      element.style.fontSize = 'larger';
-      element.style.lineHeight = '2';
+      element.style.fontFamily = "Lexend";
+      element.style.fontSize = "larger";
+      element.style.lineHeight = "2";
     });
   }
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
- 
-
-  if (request.action === 'focus_on') {
-    console.log('focus On');
-
-    
-    const textElements = document.querySelectorAll('p');
-    textElements.addEventListener('mouseenter', () => {
-      textElements.style.backgroundColor = 'purple';
-    });
-
-    textElements.addEventListener('mouseleave', () => {
-      textElements.style.backgroundColor = '';
-    });
+  if (request.action === "focus_on") {
+    console.log("focus On");
   }
 });
